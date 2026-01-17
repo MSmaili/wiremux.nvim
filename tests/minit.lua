e850782b98129c4f38b787972e8c2c8060bb7361
Muhamet Smaili <51492002+MSmaili@users.noreply.github.com>
2026-01-17 10:47:28 +0200

    add test setup

---
+++ tests/minit.lua
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S nvim -l
+
+vim.env.LAZY_STDPATH = ".tests"
+vim.env.LAZY_PATH = vim.fs.normalize("~/projects/lazy.nvim")
+
+if vim.fn.isdirectory(vim.env.LAZY_PATH) == 1 then
+	loadfile(vim.env.LAZY_PATH .. "/bootstrap.lua")()
+else
+	load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"), "bootstrap.lua")()
+end
+
+require("lazy.minit").setup({
+	spec = {
+		"echasnovski/mini.test",
+		{
+			dir = vim.uv.cwd(),
+			opts = {},
+		},
+	},
+})
+
+vim.opt.rtp:append(vim.fn.stdpath("data") .. "/site")
